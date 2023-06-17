08ee8ca29dbb1c6573d729670f944a62c0cf5ad1
Bradford <bzlee@ucdavis.edu>
2023-06-16 21:14:31 -0700

    created sql files

---
+++ server.js
@@ -0,0 +1,23 @@
+// Import and require mysql2
+const express = require('express');
+const msysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: 'mysql',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+  );
